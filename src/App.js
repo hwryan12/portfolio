b58322e43fdd9f2b48019a8dc8395cbffb8eb057
Harrison Ryan <hwryan12@gmail.com>
2023-05-11 16:23:16 -0500

    feature: Implement logic in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Project from './components/Project';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Project id="project1" title="Project 1" description="Description of Project 1..." image="path_to_project1_wireframe" />
+            <Project id="project2" title="Project 2" description="Description of Project 2..." image="path_to_project2_wireframe" />
+            {/* Add more projects as needed */}
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
