3c8de46b1a75f02e1437df2e213280c44850c7fb
Harrison Ryan <hwryan12@gmail.com>
2023-05-12 07:44:19 -0500

    refactor: Breakout projects from App/js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import React from 'react';
 import Header from './components/Header';
 import Project from './components/Body';
 import Footer from './components/Footer';
+import projects from './projects';  // import the projects array
 
 function App() {
-    const projects = [
-        {id: "project1", title: "Project 1", description: "Description of Project 1...", image: "path_to_project1_wireframe"},
-        {id: "project2", title: "Project 2", description: "Description of Project 2...", image: "path_to_project2_wireframe"},
-        // Add more projects as needed
-    ];
-
     return (
         <div className="App">
             <Header />
---
+++ src/projects.js
@@ -0,0 +1,7 @@
+const projects = [
+  {id: "project1", title: "Project 1", description: "Description of Project 1...", image: "path_to_project1_wireframe"},
+  {id: "project2", title: "Project 2", description: "Description of Project 2...", image: "path_to_project2_wireframe"},
+  // Add more projects as needed
+];
+
+export default projects;
