076380cb156aac77382977ea0169bcf5b83a944e
Harrison Ryan <hwryan12@gmail.com>
2023-05-20 17:09:26 -0500

    docs: Work on styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import About from './About';
+import styles from './Header.module.css';
 
 const Header = () => {
     const [show, setShow] = useState(false);
@@ -15,6 +16,7 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ml-auto">
+                    <Navbar.Text className={`${styles.ml3} ${styles.mr3} ${styles.boldText}`}>Harrison Ryan</Navbar.Text>
                         <NavDropdown title="Portfolio" id="collasible-nav-dropdown">
                             <NavDropdown.Item as={Link} to="/school-projects">School Projects</NavDropdown.Item>
                             <NavDropdown.Item as={Link} to="/personal-projects">Personal Projects</NavDropdown.Item>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 import styles from './Welcome.module.css'; 
 
 const Welcome = () => {
@@ -7,6 +9,11 @@ const Welcome = () => {
             <div className={styles.textContainer}>
                 <h1>Coding and design, done with love</h1>
                 <h3>Software Developer, Backend Engineer, and LEGO aficionado</h3>
+                <div className={styles.linksContainer}>
+                    <Button className="bg-dark text-white m-2 custom-shadow" as={Link} to="/projects">See my projects</Button>
+                    <Button className="bg-dark text-white m-2 custom-shadow" as={Link} to="/projects">About me</Button>
+                    <Button className="bg-dark text-white m-2 custom-shadow" as={Link} to="/projects">Thoughts</Button>
+                </div>
             </div>
         </div>
     );
