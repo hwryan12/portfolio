e3b480313bebbe7c6a5964763d3578f20c7215a8
Harrison Ryan <hwryan12@gmail.com>
2023-05-22 15:24:47 -0500

    refactor: Improve styling for

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
-import styles from './Body.module.css';  // import a CSS module
+import styles from './Body.module.css'; 
 
 const Project = ({id, title, description, images}) => {
-    const [selectedImage, setSelectedImage] = useState(images[0]);  // set the first image as the selected image initially
+    const [selectedImage, setSelectedImage] = useState(images[0]);  
 
-    // function to handle clicking on a thumbnail
     const handleImageSelect = (image) => {
         setSelectedImage(image);
     }
@@ -13,20 +12,18 @@ const Project = ({id, title, description, images}) => {
         <div id={id} className={`${styles.container} container my-5`}>
             <div className="row">
                 <div className="col-lg-6">
-                    <h2 className="mb-3">{title}</h2>
-                    <p className="text-muted">{description}</p>
+                    <h2 className={styles.h2}>{title}</h2>
+                    <p className={styles.p}>{description}</p>
                 </div>
                 <div className="col-lg-6">
                     <img src={selectedImage} alt={`${title} wireframe`} className="img-fluid rounded shadow" />
                     <div className="mt-3">
-                        {/* Render a thumbnail for each image */}
                         {images.map((image, index) => (
                             <img
                                 key={index}
                                 src={image}
                                 alt={`${title} wireframe ${index + 1}`}
-                                className="img-fluid rounded shadow m-2"
-                                style={{ width: "100px", cursor: "pointer" }}
+                                className={`img-fluid rounded shadow m-2 ${styles.thumbnail} ${image === selectedImage ? styles.active : ''}`}
                                 onClick={() => handleImageSelect(image)}
                             />
                         ))}
